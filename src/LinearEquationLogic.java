338bcb6d69882cf1dbe631e4e7be4edf164df047
MarsonMai <145681383+MarsonMai@users.noreply.github.com>
2023-10-22 14:19:59 -0400

    Update LinearEquationLogic.java

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,58 +1,50 @@
 import java.util.Scanner;
-
-
 public class LinearEquationLogic {
-private Scanner myScanner;
-private LinearEquation linearEquation;
-public LinearEquationLogic() {
-linearEquation = null;
-myScanner = new Scanner(System.in);
-
-
+    private Scanner myScanner;
+    private LinearEquation linearEquation;
+    public LinearEquationLogic() {
+        linearEquation = null;
+        myScanner = new Scanner(System.in);
+    }
+    public void start() {
+        GetInfo();
+    }
+    private void GetInfo() {
+        String x = "y";
+        while (x.contains("y")) {
+            System.out.println("Welcome to the Linear equation calculator");
+            System.out.print("Enter coordinate one: ");
+            String pointOne = myScanner.nextLine();
+            System.out.print("Enter coordinate two: ");
+            String pointTwo = myScanner.nextLine();
+            int len1 = pointOne.length();
+            int len2 = pointTwo.length();
+            int half1 = pointOne.indexOf(",");
+            int half2 = pointTwo.indexOf(",");
+            String sub1 = pointOne.substring(1,half1);
+            String sub2 = pointOne.substring(half1 + 1,len1 - 1);
+            String sub3 = pointTwo.substring(1,half2);
+            String sub4 = pointTwo.substring(half2 + 1,len2 - 1);
+            int x1 = Integer.parseInt(sub1);
+            int y1 = Integer.parseInt(sub2);
+            int x2 = Integer.parseInt(sub3);
+            int y2 = Integer.parseInt(sub4);
+            linearEquation = new LinearEquation(x1, y1, x2, y2);
+            System.out.println(linearEquation.LineInfo());
+            System.out.println(" ");
+        if (x1 != x2) {
+            System.out.print("Enter a value for x: ");
+            int newX = myScanner.nextInt();
+            System.out.println(linearEquation.coordinateForX(newX));
+            System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+            x = myScanner.nextLine();
+        } else {
+            System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+            x = myScanner.nextLine();
+        }
+        if (x.equals("n")) {
+            System.out.println("Goodbye!");
+        }
+        }
+    }
 }
-public void start() {
-GetInfo();
-
-
-}
-private void GetInfo() {
-String x = "y";
-while (x.equals("y")) {
-System.out.println("Welcome to the Linear equation calculator");
-System.out.print("Enter coordinate one: ");
-String pointOne = myScanner.nextLine();
-System.out.print("Enter coordinate two: ");
-String pointTwo = myScanner.nextLine();
-int len1 = pointOne.length();
-int len2 = pointTwo.length();
-int half1 = pointOne.indexOf(",");
-int half2 = pointTwo.indexOf(",");
-String sub1 = pointOne.substring(1,half1);
-String sub2 = pointOne.substring(half1 + 1,len1 - 1);
-String sub3 = pointTwo.substring(1,half2);
-String sub4 = pointTwo.substring(half2 + 1,len2 - 1);
-int x1 = Integer.parseInt(sub1);
-int y1 = Integer.parseInt(sub2);
-int x2 = Integer.parseInt(sub3);
-int y2 = Integer.parseInt(sub4);
-linearEquation = new LinearEquation(x1, y1, x2, y2);
-System.out.println(linearEquation.LineInfo());
-System.out.println(" ");
-if (x1 != x2) {
-System.out.print("Enter a value for x: ");
-int newX = myScanner.nextInt();
-System.out.println(linearEquation.coordinateForX(newX));
-System.out.print("Would you like to enter another pair of coordinates? y/n: ");
-x = myScanner.nextLine();
-} else {
-System.out.print("Would you like to enter another pair of coordinates? y/n: ");
-x = myScanner.nextLine();
-}
-}
-}
-
-
-
-
-}
-
